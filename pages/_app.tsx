6c8a6992173f92096ce6745904004668da2ce032
thrila <ifeanyichukwundubuizu@gmail.com>
2022-06-15 01:58:08 +0100

    i made the forms needed for the events page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,12 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import Homepage from './Homepage'
 import EventsPage from './EventsPage'
+import EventsInput from './EventsInput'
 
 function App() {
   return (
-  //<Homepage />
-<EventsPage />
-
+  
+<EventsInput />
 
 
   );
